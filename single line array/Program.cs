9caeac6a8253db5c3864b0e50327b76cd8948d8e
Kristen Shea <kstar61@live.com>
2018-08-31 20:44:43 -0500

    array with loop to print values to console.

--- single line array/Program.cs
+++ single line array/Program.cs
@@ -6,7 +6,19 @@ namespace single_line_array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            //array with specific values initialized in one line
+            int[] numbers = new int[] { 1, 1, 2, 3, 5, 8 };
+            
+            //loop that goes through and prints out each value in the array above
+            foreach (int number in numbers)
+            {
+                Console.WriteLine($"Value: {number}");
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
