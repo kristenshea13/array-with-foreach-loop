d4db8961ef21980368a252a940611f5da92f05d3
Kristen Shea <kstar61@live.com>
2018-08-31 20:24:46 -0500

    Add project files.

---
+++ single line array/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace single_line_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
